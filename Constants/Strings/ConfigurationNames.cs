8d151ec129f5a094ed2268b8b16e3abc5e2f8279
anloffredo <anloffredo@massageenvy.com>
2020-05-14 19:36:23 -0700

    Auto Commit

--- Constants/Strings/ConfigurationNames.cs
+++ Constants/Strings/ConfigurationNames.cs
@@ -1,7 +1,11 @@
 ﻿namespace Penguin.Cms.Modules.Files.Constants.Strings
 {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
     public static class ConfigurationNames
     {
         public const string USER_FILES_ROOT = "UserFilesRoot";
     }
+
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
